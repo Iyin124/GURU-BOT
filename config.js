498dde8c4a9eb3af3c321e82fef74169e8e4a432
Iyin124 <147757042+Iyin124@users.noreply.github.com>
2023-10-25 08:03:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['2349123247256'Iyin , true],
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Iyin Bot'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
